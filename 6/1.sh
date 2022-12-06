b729f3e38981ea6988b616aa3039fb137c9b5624
Ryan Hadley <rhadley@recurly.com>
2022-12-05 22:50:04 -0700

    simplified

--- 6/1.sh
+++ 6/1.sh
@@ -5,22 +5,15 @@ marker=""
 
 for (( i=0; i<${#signal}; i++ ))
 do
-  marker="${marker}${signal:$i:1}"
-  if [[ ${#marker} -eq 5 ]]
+  marker="${signal:$i:4}"
+  unique=$(for (( j=0; j<4; j++))
+  do
+    echo "${marker:$j:1}"
+  done | sort | uniq | wc -l)
+  if [[ "$unique" -eq 4 ]]
   then
-    marker="${marker:1}"
-  fi
-  if [[ ${#marker} -eq 4 ]]
-  then
-    unique=$(for (( j=0; j<4; j++))
-    do
-      echo "${marker:$j:1}"
-    done | sort | uniq | wc -l)
-    if [[ "$unique" -eq 4 ]]
-    then
-      echo "$marker"
-      echo $((i + 1))
-      break
-    fi
+    echo "$marker"
+    echo $((i + 4))
+    break
   fi
 done
--- 6/2.sh
+++ 6/2.sh
@@ -5,22 +5,15 @@ marker=""
 
 for (( i=0; i<${#signal}; i++ ))
 do
-  marker="${marker}${signal:$i:1}"
-  if [[ ${#marker} -eq 15 ]]
+  marker="${signal:$i:14}"
+  unique=$(for (( j=0; j<14; j++))
+  do
+    echo "${marker:$j:1}"
+  done | sort | uniq | wc -l)
+  if [[ "$unique" -eq 14 ]]
   then
-    marker="${marker:1}"
-  fi
-  if [[ ${#marker} -eq 14 ]]
-  then
-    unique=$(for (( j=0; j<14; j++))
-    do
-      echo "${marker:$j:1}"
-    done | sort | uniq | wc -l)
-    if [[ "$unique" -eq 14 ]]
-    then
-      echo "$marker"
-      echo $((i + 1))
-      break
-    fi
+    echo "$marker"
+    echo $((i + 14))
+    break
   fi
 done
