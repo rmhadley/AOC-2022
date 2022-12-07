249eaa345184cabd725437774f20e73928a57a18
Ryan Hadley <rhadley@recurly.com>
2022-12-06 23:45:23 -0700

    Day 6 parts 1 and 2

---
+++ 2022/7/1.sh
@@ -0,0 +1,49 @@
+#!/usr/local/bin/bash
+
+top_dir=$(pwd)
+base_dir="./fs"
+rm -rf "$base_dir"
+mkdir "$base_dir"
+cd "$base_dir"
+ls_output=0
+
+while read -r terminal
+do
+  if [[ "$ls_output" -eq 1 ]]
+  then
+    if [[ ! "$terminal" =~ ^\$.*$ ]]
+    then
+      if [[ "$terminal" =~ ^dir.*$ ]]
+      then
+        mkdir "${terminal/* /}"
+      else
+        echo "${terminal/ */}" > "${terminal/* /}"
+      fi
+    else
+      ls_output=0
+    fi
+  fi
+
+  if [[ "$ls_output" -eq 0 ]]
+  then
+    if [[ "$terminal" =~ ^\$.*cd.*$ ]]
+    then
+      cd ./"${terminal/* /}"
+    elif [[ "$terminal" =~ ^\$.*ls$ ]]
+    then
+      ls_output=1
+    fi
+  fi
+done < ../input
+
+total=0
+for directory in $(find "${top_dir}/${base_dir}" -type d)
+do
+  dir_size=$(find "$directory" -type f -exec cat {} \; | jq -s add)
+  if [[ "$dir_size" -le 100000 ]]
+  then
+    total=$((total + dir_size))
+  fi
+done
+
+echo "$total"
